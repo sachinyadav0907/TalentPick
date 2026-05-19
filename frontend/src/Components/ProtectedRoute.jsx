739949fd5c491c3f774a161d8bd2a564f4b04c40
sachinyadav0907 <ysachin8600@gmail.com>
2026-05-19 20:33:44 +0530

    improve protected Routes

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -2,5 +2,10 @@ import Unauthorized from "./Unauthorized";
 
 export default function ProtectedRoute({ children }) {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Unauthorized/>
+  }
+
   return children;
 }
