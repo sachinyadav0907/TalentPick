07a1175094dbab9fd46c5b6b6fb9d6a8cd76f5d8
sachinyadav0907 <ysachin8600@gmail.com>
2026-05-27 23:06:55 +0530

    feat: Add show or hide button on password

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,28 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar.jsx";
 import Footer from "../Components/Footer.jsx";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FiEyeOff } from "react-icons/fi";
+import { FiEye } from "react-icons/fi";
+import axios from "axios";
+
+const loginSchema = z.object({
+  role: z.enum(["jobseeker", "recruiter"], {
+    required_error: "role is required",
+  }),
+  email: z.string().email("Invalid Email"),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(32, "Password must not exceed 32 characters")
+    .regex(/[A-Z]/, "Must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Must contain at least one lowercase letter")
+    .regex(/[0-9]/, "Must contain at least one number")
+    .regex(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      "Must contain at least one special character",
+    ),
+});
 
 function Login() {
+  const [eyeOpen, setEyeOpen] = useState(true);
+  const eyeToggle = () => {
+    setEyeOpen(!eyeOpen);
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const formSubmit = async (data) => {
+    try {
+      const useerInfo = await axios.post("http://localhost:5000/auth/login", data,{
+        withCredentials: true,
+        headers:{
+          "Content-Type": "application/json"
+        }
+      });
+      console.log(useerInfo);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100">
       <Navbar />
 
       <div className="flex items-center justify-center px-4 py-10">
         <div className="w-full max-w-md rounded-2xl border border-white/10 bg-slate-900/80 p-8 shadow-2xl backdrop-blur-xl">
-
           <div className="mb-8 text-center">
-            <h1 className="text-4xl font-bold text-white">
-              Welcome Back
-            </h1>
+            <h1 className="text-4xl font-bold text-white">Welcome Back</h1>
 
             <p className="mt-2 text-slate-300">
               Login to continue your journey
             </p>
           </div>
 
-          <form className="flex flex-col gap-5">
-
+          <form
+            className="flex flex-col gap-5"
+            onSubmit={handleSubmit(formSubmit)}
+          >
             <div>
               <label
                 htmlFor="role"
@@ -32,12 +81,19 @@ function Login() {
               </label>
 
               <select
-                id="role"
                 className="w-full rounded-xl border border-white/10 bg-[#0B1120] px-3 py-3 text-slate-100 outline-none focus:border-violet-500"
+                {...register("role", { required: "role is required " })}
               >
-                <option className="bg-[#0B1120]">Job Seeker</option>
-                <option className="bg-[#0B1120]">Recruiter</option>
+                <option value="jobseeker" className="bg-[#0B1120]">
+                  Job Seeker
+                </option>
+                <option value="recruiter" className="bg-[#0B1120]">
+                  Recruiter
+                </option>
               </select>
+              {errors.role && (
+                <p className="text-red-400 mx-2 my-1">{errors.role.message}</p>
+              )}
             </div>
 
             <div>
@@ -50,11 +106,13 @@ function Login() {
 
               <input
                 type="email"
-                id="email"
                 placeholder="Enter your email"
-                required
+                {...register("email", { required: "Email is required" })}
                 className="w-full rounded-xl border border-white/10 bg-[#0B1120] px-3 py-3 text-slate-100 outline-none placeholder:text-slate-400 focus:border-violet-500"
               />
+              {errors.email && (
+                <p className="text-red-400 mx-2 my-1">{errors.email.message}</p>
+              )}
             </div>
 
             <div>
@@ -65,13 +123,33 @@ function Login() {
                 Password
               </label>
 
-              <input
-                type="password"
-                id="password"
-                placeholder="Enter your password"
-                required
-                className="w-full rounded-xl border border-white/10 bg-[#0B1120] px-3 py-3 text-slate-100 outline-none placeholder:text-slate-400 focus:border-violet-500"
-              />
+              <div className="flex items-center w-full rounded-xl border border-white/10 bg-[#0B1120] px-3 py-3 focus-within:border-violet-500">
+                <input
+                  id="password"
+                  type={eyeOpen ? "text" : "password"}
+                  placeholder="Enter your password"
+                  className="w-full bg-transparent text-slate-100 outline-none placeholder:text-slate-400"
+                  {...register("password")}
+                />
+
+                {eyeOpen ? (
+                  <FiEye
+                    onClick={eyeToggle}
+                    className="cursor-pointer text-xl text-slate-300"
+                  />
+                ) : (
+                  <FiEyeOff
+                    onClick={eyeToggle}
+                    className="cursor-pointer text-xl text-slate-300"
+                  />
+                )}
+              </div>
+
+              {errors.password && (
+                <p className="mx-2 my-1 text-sm text-red-400">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
 
             <div className="flex justify-end">
@@ -89,7 +167,6 @@ function Login() {
             >
               Login
             </button>
-
           </form>
 
           <div className="mt-6 text-center">
@@ -103,7 +180,6 @@ function Login() {
               </Link>
             </p>
           </div>
-
         </div>
       </div>
 
