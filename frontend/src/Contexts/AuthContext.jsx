9bd9be01f2358ec05bb0e72e8933148963f3f0a6
sachinyadav0907 <ysachin8600@gmail.com>
2026-05-28 21:42:35 +0530

    refactor: Auth context api

--- frontend/src/Contexts/AuthContext.jsx
+++ frontend/src/Contexts/AuthContext.jsx
@@ -1,14 +1,27 @@
 import { createContext , useContext} from "react";
 import {jwtDecode} from "jwt-decode";
+import { useEffect } from "react";
+import { useState } from "react";
+import { json } from "zod";
  
 const AuthContext = createContext();
 
  export const AuthProvider =({children})=>{
-  const token = localStorage.getItem("token");
-  const tokenValue = {role:"Recruiter"};
-  const isRecruiter = tokenValue.role === "Recruiter";
+
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    const userInfo = localStorage.getItem("userInfo");
+
+    if(userInfo){
+      const parsedUserInfo = JSON.parse(userInfo);
+
+      setUser(parsedUserInfo);
+    }
+  },[])
+  const isRecruiter = user?.role === "recruiter";
    return (
-    <AuthContext.Provider value={isRecruiter}>
+    <AuthContext.Provider value={{user, isRecruiter}}>
       {children}
     </AuthContext.Provider>
   );
