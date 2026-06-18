4ef1908550cd4bb7da169aace93bfb619c2ea1bd
sachinyadav0907 <ysachin8600@gmail.com>
2026-06-18 13:41:45 +0530

    Feat: Create application model for apply job api

---
+++ backend/Model/applicationModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const applicationSchema = new mongoose.Schema(
+  {
+    jobId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
+      index: true,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+applicationSchema.index(
+  { jobId: 1, userId: 1 },
+  { unique: true }
+);
+
+const Application = mongoose.model("Application", applicationSchema);
+
+export default Application;
