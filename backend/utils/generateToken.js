d5ce9f758fd3de16236da2c595e358e2d8e6a509
sachinyadav0907 <ysachin8600@gmail.com>
2026-07-04 20:35:18 +0530

    fix: increase token time from 1h to 7d

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user)=>{
-  return jwt.sign({id:user.id, role:user.role},process.env.JWT_SECRET, {expiresIn: "1h"});
+  return jwt.sign({id:user.id, role:user.role},process.env.JWT_SECRET, {expiresIn: "7d"});
 }
